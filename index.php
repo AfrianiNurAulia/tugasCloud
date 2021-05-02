e51c645f28f946b862214527e62989e2aef01659
AfrianiNurAulia <auliaafrianinur0104@gmail.com>
2021-05-02 21:40:30 +0800

    cloud

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Heroku</title>
+</head>
+<body>
+<?php
+	echo "Nama    : Afriani Nur Aulia";
+	echo "</br>";
+	echo "Kelas   : E(TI)";
+	echo "</br>";
+	echo "Stambuk : 182203";
+?>
+</body>
+</html>
